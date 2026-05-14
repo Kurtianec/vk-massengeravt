8c8d165bb35e69ad6a266a7b81c527163740b418
Kurtianec <ananas.kuropatka@gmail.com>
2026-05-14 13:25:56 +0300

    del

--- keep-alive.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-while true; do
-  cd /home/z/my-project
-  NODE_ENV=production node .next/standalone/server.js
-  echo "Server died, restarting in 2 seconds..." >> /home/z/my-project/server-restart.log
-  sleep 2
-done
